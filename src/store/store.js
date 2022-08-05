40cb33b4792169a5a7a0ea4ddd4cb4e3d4289e39
Seabagel <hsrt1367tim@gmail.com>
2022-08-04 18:22:35 -0700

    [Data] I forgot what I did

--- src/store/store.js
+++ src/store/store.js
@@ -16,19 +16,24 @@ export default createStore({
         ...state.allUsers.map((user) => {
           // 👇️ if id equals params._employeeID replace object
           if (user.employeeID === params.employeeID) {
-            user.schedules[params.schedule][params.biWeekly] = !user.schedules[
-              params.schedule
-            ][params.biWeekly];
+            user.schedules[params.schedule][params.biWeekly] =
+              !user.schedules[params.schedule][params.biWeekly];
           }
           // 👇️ otherwise return object as is
           return user;
         }),
       ];
     },
+    replaceAllUsers(state, params) {
+      state.allUsers = params;
+    },
   },
   actions: {
     async actScheduleMorning({ commit }, payload) {
       commit("mutateScheduleMorning", payload);
+    },
+    async actDBGetUsers({ commit }, payload) {
+      commit("replaceAllUsers", payload);
     },
   },
 });
